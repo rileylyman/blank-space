1b6be1b38b283de018252b522184a2db59fd65d3
Riley Lyman <rileylyman@berkeley.edu>
2024-01-06 11:35:27 -0800

    get basic layout done for both mobile and desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,10 +38,7 @@
 </script>
 
 <div id="root-container">
-    <div id="settings"> settings </div>
     <div id="header"> Blank Space </div>
-    <div id="help"> help </div>
-    <div/>
     <input 
         class:correct-guess={correctGuess} 
         type="text" 
@@ -50,73 +47,95 @@
         disabled={correctGuess}
         placeholder={placeholder}
         />
-    <div/>
-    <div/>
     <div id="score"> <p class="score-label">Score:</p> 25 </div>
-    <div/>
-    <div/>
-    <p id="hint-help">Click a hint to reveal</p>
-    <div/>
-    {#each hints as {revealed, cost, value}, hintIdx}
-        {#if ((hintIdx % 2) == 0)}
-            <div/>
-        {/if}
-        <div class="hint" class:revealed on:click={revealHint(hintIdx)}>
-            <div class="back">{value}</div>
-            <div class="front">-{cost} point{cost > 1 ? 's' : ''}</div>
-        </div>
-        {#if ((hintIdx %2) == 1)}
-            <div/>
-        {/if}
-    {/each}
-    <div/>
-    <p id="hint-help">Incorrect guesses:</p>
-    <div/>
-    <div/>
+    <p id="hints-label">Click a hint to reveal:</p>
+    <div class="hints">
+        {#each hints as {revealed, cost, value}, hintIdx}
+            <div class="hint" 
+                class:hint-left={(hintIdx % 2) == 0} 
+                class:hint-right={(hintIdx % 2) == 1} 
+                class:revealed 
+                on:click={revealHint(hintIdx)}
+                >
+                <div class="back">{value}</div>
+                <div class="front">-{cost} point{cost > 1 ? 's' : ''}</div>
+            </div>
+        {/each}
+    </div>
+    <p id="incorrect-guesses-label">Incorrect guesses:</p>
     <div id="incorrect-guesses">
         {#each incorrectGuesses as g}
             <p>{g}</p>
         {/each}
     </div>
-    <div/>
 </div>
 
 <style>
     #root-container {
-        margin-top: 1.5rem;
         display: grid;
-        grid-template-columns: 1fr 2fr 2fr 1fr;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 2fr 2fr 4rem 0.5fr 4fr 0.5fr 3fr;
+        grid-template-areas: "header header"
+              "score score"
+              "guess guess"
+              "hints-label hints-label"
+              "hints hints"
+              "incorrect-guesses-label incorrect-guesses-label"
+              "incorrect-guesses incorrect-guesses";
+        grid-auto-rows: min-content;
+        grid-auto-flow: dense;
         justify-items: center;
         align-items: center;
         column-gap: 1rem;
         row-gap: 1rem;
         font-family: 'Verdana';
         width: 100vw;
+        height: 100vh;
+        width: 100svw;
+        height: 100svh;
+        padding-bottom: 2rem;
+        padding-top: 0rem;
     }
 
-    #hint-help {
-        font-size: 1.3rem;
-        grid-column: span 2;
-
+    @media screen and (min-width: 900px) {
+        #root-container {
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr 1fr 4rem 0.5fr 1fr 1fr 1fr 1fr;
+            grid-template-areas: "header header"
+                "score score"
+                "guess guess"
+                "hints-label incorrect-guesses-label"
+                "hints incorrect-guesses"
+                "hints incorrect-guesses"
+                "hints incorrect-guesses"
+                "hints incorrect-guesses"
+        }
     }
 
     #header {
-        grid-column: span 2;
-        font-size: 5rem;
+        font-size: 12vw;
         text-decoration: underline overline;
+        grid-area: header;
+    }
+
+    @media screen and (min-width: 640px) {
+        #header {
+            font-size: 5rem;
+        }
     }
     
     input {
         all: unset;
-        grid-column: span 2;
+        margin-top: 1rem;
         text-transform: uppercase;
-        margin: 2rem 0 2rem 0;
-        width: 80%;
-        height: 4rem;
+        align-self: stretch;
+        min-width: 50%;
+        max-height: 3rem;
         text-align: center;
         border-radius: 0.5rem;
         border: 1px solid black;
         font-size: 1.5em;
+        grid-area: guess;
     }
     
     input:disabled {
@@ -134,33 +153,65 @@
 
     #score {
         position: relative;
-        grid-column: span 2;
-        font-size: 5rem;
+        grid-area: score;
+        font-size: 4rem;
+        padding: 1rem;
         background: lightgrey;
         border-radius: 50%;
-        width: 10rem;
-        height: 10rem;
+        align-self: stretch;
+        height: 100%;
+        aspect-ratio: 1 / 1;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
     }
 
+    @media screen and (min-width: 900px) {
+        #score {
+            font-size: 10vw;
+        }
+
+    }
+
     #score > .score-label {
         position: absolute;
         display: block;
-        font-size: 1rem;
+        font-size: 0.8rem;
         top: 1rem;
     }
 
+    #hints-label {
+        font-size: 1rem;
+        grid-area: hints-label;
+        margin-bottom: -1rem;
+    }
+
+    .hints {
+        min-height: 10rem;
+        align-self: stretch;
+        justify-self: stretch;
+        grid-area: hints;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        row-gap: 1rem;
+        column-gap: 1rem;
+        padding: 0 5% 0 5%;
+    }
+
+    @media screen and (min-width: 900px) {
+        .hints {
+            grid-template-columns: 1fr;
+        }
+    }
+
     .hint {
-        font-size: 1.5em;
+        font-size: min(2vh, 5vw);
         text-transform: uppercase;
         position: relative;
         text-align: center;
-        min-width: 100%;
-        height: 10rem;
-        justify-self: center;
+        align-self: stretch;
+        justify-self: stretch;
     }
 
     .hint:hover {
@@ -201,12 +252,19 @@
         transform: rotateX(180deg);
     }
 
+    #incorrect-guesses-label {
+        grid-area: incorrect-guesses-label;
+        margin-bottom: -1rem;
+    }
+
     #incorrect-guesses {
-        grid-column: span 2;
-        width: 80%;
-        height: 20rem;
+        grid-area: incorrect-guesses;
+        width: 90%;
+        align-self: stretch;
         border: 1px solid black;
         border-radius: 0.5rem;
+        white-space: nowrap;
+        text-overflow: ellipsis;
         overflow-x: hidden;
         overflow-y: scroll;
         text-align: center;
