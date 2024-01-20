394aba0aaf51a7890389bc62e1be779aa45ca3f7
Riley Lyman <rileylyman@berkeley.edu>
2024-01-20 15:36:12 -0800

    continue work on new card layout

--- src/routes/games/blankspace/[gameid]/next/+page.svelte
+++ src/routes/games/blankspace/[gameid]/next/+page.svelte
@@ -21,29 +21,32 @@
         }
     });
 
-    let scrollContainer: HTMLDivElement | null = null;
-    let screenWidth = 0;
+    let currentHint = 0;
     const handleInput = (e: Event & { currentTarget: HTMLInputElement }) => {
-        if (!scrollContainer) return;
-        scrollContainer.scrollLeft += screenWidth;
+        currentHint += 1;
     }
+    let reveal = true;
+    let showHint = [false, false, false, false, false];
 </script>
-
-<svelte:window bind:outerWidth={screenWidth} />
 
 <div id="root">
     <div>
-        <p> Your Guesses: </p>
     </div>
-    <div
-        class="card-container"
-        bind:this={scrollContainer}
-        on:touchmove|preventDefault={() => {}}
-        on:scroll|preventDefault={() => {}}
-    >
-        {#each availHints as { hint }}
-            <div class="card">
-                <input on:change={handleInput} type="text" placeholder={hint} />
+    <div class="card-container">
+        {#each availHints as { hint }, idx}
+            <div 
+                class="card"
+                class:away={currentHint < idx}
+                class:revealed={idx <= currentHint && reveal}
+                class:current={currentHint === idx}
+                class:hint-shown={showHint[idx]}
+            >
+                <div class="hint-side">
+                    <button on:click={() => showHint[idx] = false}> Show Back </button>
+                </div>
+                <div class="back-side">
+                    <button on:click={() => showHint[idx] = true}> Show Hint </button>
+                </div>
             </div>
         {/each}
     </div>
@@ -52,36 +55,126 @@
 <style>
     #root {
         height: 100vh;
-        height: 100svh;
         display: grid;
-        grid-template-rows: 0.5fr 1.3fr 1fr 1fr;
+        grid-template-rows: 5rem 10rem 1fr 1fr;
         overflow: hidden;
-        place-items: center;
+        place-items: stretch;
     }
 
     .card-container {
-        height: 100%; 
-        width: 100%; 
         display: grid;
         grid-template-columns: repeat(5, 1fr);
-        overflow-x: scroll;
-        scroll-snap-type: x mandatory;
-        scrollbar-width: none;
-        scroll-behavior: smooth;
-    }
-
-    .card-container::-webkit-scrollbar {
-        display: none;
+        position: relative;
     }
 
     .card {
-        background: lightgrey;
-        border-radius: 0.5rem;
-        border: 1px solid black;
+        position: absolute;
         display: grid;
-        place-items: center;
+        align-items: start;
+        justify-items: center;
         width: 90vw;
         margin: 0 5vw;
-        height: 90%;
+        height: 100%;
+        transform-style: preserve-3d;
+        transition: transform 1s;
+    }
+
+    .card.current {
+        background: white;
+    }
+
+    .hint-side, .back-side {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+        border: 1px solid black;
+        border-radius: 0.5rem;
+        display: grid;
+        transition: transform 500ms;
+        place-items: center;
+        z-index: 2;
+    }
+
+    .hint-side {
+        transform: rotateX(180deg);
+        background: white;
+    }
+
+    .back-side {
+        transform: rotateX(0deg);
+        background: #f0f0f0;
+    }
+
+    .hint-shown > .hint-side {
+        transform: rotateX(0deg);
+    }
+
+    .hint-shown > .back-side {
+        transform: rotateX(180deg);
+    }
+
+    .card.away {
+        transform: translateX(100vw);
+    }
+
+    .card:nth-child(1) {
+    }
+
+    .card:nth-child(1).away {
+        transform: translateX(100vw);
+    }
+
+    .card:nth-child(1).revealed {
+        transition: transform 300ms;
+        transform: translateY(-25%);
+    }
+
+    .card:nth-child(2) {
+    }
+
+    .card:nth-child(2).away {
+        transform: translateY(10%) translateX(100vw);
+    }
+
+    .card:nth-child(2).revealed {
+        transition: transform 300ms;
+        transform: translateY(0%);
+    }
+
+    .card:nth-child(3) {
+    }
+
+    .card:nth-child(3).away {
+        transform: translateY(20%) translateX(100vw);
+    }
+
+    .card:nth-child(3).revealed {
+        transition: transform 300ms;
+        transform: translateY(25%);
+    }
+
+    .card:nth-child(4) {
+    }
+
+    .card:nth-child(4).away {
+        transform: translateY(30%) translateX(100vw);
+    }
+
+    .card:nth-child(4).revealed {
+        transition: transform 300ms;
+        transform: translateY(50%);
+    }
+
+    .card:nth-child(5) {
+    }
+
+    .card:nth-child(5).away {
+        transform: translateY(40%) translateX(100vw);
+    }
+
+    .card:nth-child(5).revealed {
+        transition: transform 300ms;
+        transform: translateY(75%);
     }
 </style>
