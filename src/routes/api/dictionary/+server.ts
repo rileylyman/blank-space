8efc0ccfc7270270ff27e487e7c9f2afb3329c73
unknown <rileylyman@gmail.com>
2024-01-25 07:34:47 -0800

    wip dictionary file

--- src/routes/api/dictionary/+server.ts
+++ src/routes/api/dictionary/+server.ts
@@ -1,13 +1,17 @@
 import { json, type RequestEvent } from "@sveltejs/kit"
 
+import fs from 'fs';
+
+let contents = fs.readFileSync("./src/routes/api/dictionary/words.txt").toString().split('\n');
+console.log(contents[100]);
+
 export const GET = async (event: RequestEvent) => {
+    return json({ isWord: false });
+
     const fetch = event.fetch;
     const word = event.url.searchParams.get('word');
     if (!word) {
         return json({ isWord: false });
     }
-
-    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-
-    return json({ isWord: res.status === 200 });
+    return json({ isWord: contents.includes(word.toLocaleLowerCase()) });
 }
