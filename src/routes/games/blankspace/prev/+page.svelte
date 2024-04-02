754b60f97e358d3df7ff07b7b464805ffe43e708
Riley Lyman <rileylyman@berkeley.edu>
2024-04-02 12:06:22 -0700

    forgot to commit page.svelte

---
+++ src/routes/games/blankspace/prev/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<div id="root">
+</div>
+
+<style>
+    #root {
+        width: calc(min(100vw, 50rem));
+        margin: 0 auto;
+        height: 100vh;
+        height: 100svh;
+        background: white;
+    }
+</style>
