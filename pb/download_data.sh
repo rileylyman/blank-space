61634a0021fae4fa75882c94baadbe66eec4e49c
Riley Lyman <rileylyman@berkeley.edu>
2024-04-25 17:33:39 -0700

    no longer download migrations from server

--- pb/download_data.sh
+++ pb/download_data.sh
@@ -3,13 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 host="pb"
-zipfile="/tmp/pb_data_migrations.zip"
+zipfile="/tmp/pb_data.zip"
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-ssh "$host" "rm $zipfile; zip $zipfile pb_migrations/* pb_data/data.db pb_data/logs.db pb_data/types.d.ts"
+ssh "$host" "rm $zipfile; zip $zipfile pb_data/data.db pb_data/logs.db pb_data/types.d.ts"
 scp "$host":"$zipfile" "$zipfile"
 
 mv pb_data pb_data_back || true
-mv pb_migrations pb_migrations_back || true
 unzip $zipfile
