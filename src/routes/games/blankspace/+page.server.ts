5fcd86f3ca246d7e4ac80ccfb3c0c6654185f5d8
Riley Lyman <rileylyman@berkeley.edu>
2024-06-25 11:18:33 -0700

    fix score today bug

--- src/routes/games/blankspace/+page.server.ts
+++ src/routes/games/blankspace/+page.server.ts
@@ -13,7 +13,7 @@ export const load = async (event: ServerLoadEvent) => {
         .collection('bs_this_week_scores')
         .getFullList({ fetch, filter: `user = "${userId}"` })).at(0);
 
-    const currentScore = progs.reduce((prev: number, p) => prev + p.score, 0);
+    const currentScore = progs.reduce((prev: number, p) => prev + (p.won ? p.score : 0), 0);
     const weekScore = standing?.total_score ?? 0;
 
     return {
